4a8ca2ceea6529bcb7c1aeb7d89053646c1384eb
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2021-03-10 03:49:16 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup
 
 setup(
     name='app',
-    version='0.0.1',
+    version='0.0.2',
+    description='World API',
+    long_description='World API',
+    author='Visar Zejnullahu',
+    author_email='visar.zejnullahu@gmail.com',
+    url='https://github.com/visar/world',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -16,4 +21,20 @@ setup(
         'psycopg2-binary',
         'webargs',
     ],
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+    ],
+    project_urls={
+        'Changelog': 'https://github.com/visar/world/blob/master/CHANGELOG.rst',
+        'Issue Tracker': 'https://github.com/visar/world/issues',
+    },
+    python_requires='>=3.7',
+    license='MIT',
 )
